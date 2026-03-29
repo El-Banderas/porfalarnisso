d401437b8d606496c555134de5b5060b9bf07533
El-Banderas <El-Banderas@users.noreply.github.com>
2026-03-29 19:07:27 +0000

    style: format files

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,41 +3,30 @@ import { Flex, Button } from "@mantine/core";
 import logo from "public/images/logo.png";
 import classes from "./Footer.module.css";
 export default function AboutMe() {
-
   const renderTitleSection = (title, subtitle) => {
-       return (
-      <Flex
-        direction={{ base: 'column' }}
-      >
-
+    return (
+      <Flex direction={{ base: "column" }}>
         <h1>{title}</h1>
         <p>{subtitle}</p>
-             </Flex>
+      </Flex>
     );
   };
 
   return (
     <section className={classes.section}>
-      <Flex
-        direction={{ base: 'column' }}
-      >
-          <div className={classes.img}>
+      <Flex direction={{ base: "column" }}>
+        <div className={classes.img}>
+          <Image src={logo} alt="Logo" fill />
+        </div>
+      </Flex>
 
-            <Image src={logo} alt="Logo" fill />
-            </div>
-
-             </Flex>
-
-      <Flex
-        direction={{ base: "column", xs: "row" }}
-        gap="lg"
-      >
-
-{renderTitleSection("Gabinete", "Rua de qualquer coisa, porta, Vila Nova de Gaia")}
-{renderTitleSection("Horário", "Segunda das 9h às 11h")}
-
-             </Flex>
- 
+      <Flex direction={{ base: "column", xs: "row" }} gap="lg">
+        {renderTitleSection(
+          "Gabinete",
+          "Rua de qualquer coisa, porta, Vila Nova de Gaia",
+        )}
+        {renderTitleSection("Horário", "Segunda das 9h às 11h")}
+      </Flex>
     </section>
   );
 }
