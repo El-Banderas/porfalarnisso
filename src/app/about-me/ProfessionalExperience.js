8ff41399e365c69da7fa07fba7fac901db48ac20
El-Banderas <El-Banderas@users.noreply.github.com>
2026-05-09 15:38:20 +0000

    style: format files

--- src/app/about-me/ProfessionalExperience.js
+++ src/app/about-me/ProfessionalExperience.js
@@ -1,14 +1,12 @@
 import styles from "./profExperience.module.css";
 import ProfissionalExperienceCard from "@/app/about-me/ProfessionalExperienceCard";
 
-
 export default function ProfissionalExperience() {
   return (
-    <main className={styles.main} >
-  <h1>Experiência Profissional</h1> 
-  <ProfissionalExperienceCard />
-  <section>
-  </section>
+    <main className={styles.main}>
+      <h1>Experiência Profissional</h1>
+      <ProfissionalExperienceCard />
+      <section></section>
     </main>
   );
 }
--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -3,8 +3,8 @@ import styles from "./page.module.css";
 import firstImg from "public/images/AboutMe/first.png";
 import photo from "public/images/AboutMe/photo.png";
 import logo from "public/images/logo.png";
-import FormationTable from "@/app/about-me/formationTable"
-import ProfessionalExperience from "@/app/about-me/ProfessionalExperience"
+import FormationTable from "@/app/about-me/formationTable";
+import ProfessionalExperience from "@/app/about-me/ProfessionalExperience";
 
 export default function Home() {
   return (
@@ -19,31 +19,24 @@ export default function Home() {
           />
         </div>
         <section className={`${styles.flexVertical} ${styles.section}`}>
-
           <h1>Sobre mim</h1>
           <div className={styles.horizontalFlex}>
             <div className={styles.selfImg}>
-              <Image
-                src={photo}
-                alt="Sara image"
-                className={styles.selfImg}
-              />
+              <Image src={photo} alt="Sara image" className={styles.selfImg} />
             </div>
             <FormationTable />
-            
+
             <Image
-                src={logo}
-                width={800}
-                height={550}
-                alt="logo"
-                className={styles.logo}
-                priority
-              />
-
+              src={logo}
+              width={800}
+              height={550}
+              alt="logo"
+              className={styles.logo}
+              priority
+            />
           </div>
-
         </section>
-            <ProfessionalExperience />
+        <ProfessionalExperience />
       </div>
     </main>
   );
