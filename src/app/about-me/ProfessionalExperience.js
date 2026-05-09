009be09300eb27b8c8c39ed5765ba06425336c1b
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2026-05-09 16:37:12 +0100

    Ballon not working

---
+++ src/app/about-me/ProfessionalExperience.js
@@ -0,0 +1,14 @@
+import styles from "./profExperience.module.css";
+import ProfissionalExperienceCard from "@/app/about-me/ProfessionalExperienceCard";
+
+
+export default function ProfissionalExperience() {
+  return (
+    <main className={styles.main} >
+  <h1>Experiência Profissional</h1> 
+  <ProfissionalExperienceCard />
+  <section>
+  </section>
+    </main>
+  );
+}
--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import firstImg from "public/images/AboutMe/first.png";
 import photo from "public/images/AboutMe/photo.png";
+import logo from "public/images/logo.png";
+import FormationTable from "@/app/about-me/formationTable"
+import ProfessionalExperience from "@/app/about-me/ProfessionalExperience"
 
 export default function Home() {
   return (
     <main className={styles.main} >
       <div>
-<div className={styles.imgContainer}>
+        <div className={styles.imgContainer}>
           <Image
             src={firstImg}
             alt="Office image"
@@ -15,19 +18,33 @@ export default function Home() {
             style={{ objectFit: "cover" }}
           />
         </div>
-        <section className={styles.flexVertical}>
+        <section className={`${styles.flexVertical} ${styles.section}`}>
 
-     <h1>Sobre mim</h1>
-<div className={styles.selfImg}>
-          <Image
-            src={photo}
-            alt="Sara image"
-            className={styles.selfImg}
-          />
-        </div>
- 
+          <h1>Sobre mim</h1>
+          <div className={styles.horizontalFlex}>
+            <div className={styles.selfImg}>
+              <Image
+                src={photo}
+                alt="Sara image"
+                className={styles.selfImg}
+              />
+            </div>
+            <FormationTable />
+            
+            <Image
+                src={logo}
+                width={800}
+                height={550}
+                alt="logo"
+                className={styles.logo}
+                priority
+              />
+
+          </div>
+
         </section>
-     </div>
+            <ProfessionalExperience />
+      </div>
     </main>
   );
 }
