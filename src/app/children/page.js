546a28321138a76cf90a888cf17d8d30248b1bc5
El-Banderas <El-Banderas@users.noreply.github.com>
2026-06-08 18:28:58 +0000

    style: format files

--- src/app/children/page.js
+++ src/app/children/page.js
@@ -2,26 +2,32 @@
 import styles from "./childrenPage.module.css";
 import firstImg from "public/images/AboutMe/first.png";
 import ImageWText from "@/components/ImageWText/ImageWText";
-import BtnsToRedirect from "@/components/cases/BtnsToRedirect"
+import BtnsToRedirect from "@/components/cases/BtnsToRedirect";
 
 export default function Home() {
   const sections = [
-    { "name": "Problema 1", "description": "Descrição do problema ......" }
-  ]
+    { name: "Problema 1", description: "Descrição do problema ......" },
+  ];
   return (
     <main className={styles.btnsSection}>
       <ImageWText image={firstImg} title={"Crianças"} />
       <div className={styles.content}>
-        <section  >
-          <h1 >Áreas de intervenção </h1>
+        <section>
+          <h1>Áreas de intervenção </h1>
           <BtnsToRedirect list="sections" />
         </section>
-        <section id="problema-1" style={{ marginTop: '100vh', padding: '20px' }}>
+        <section
+          id="problema-1"
+          style={{ marginTop: "100vh", padding: "20px" }}
+        >
           <h2>Problema 1</h2>
           <p>Content for the first problem goes here...</p>
         </section>
 
-        <section id="problema-2" style={{ marginTop: '100vh', padding: '20px' }}>
+        <section
+          id="problema-2"
+          style={{ marginTop: "100vh", padding: "20px" }}
+        >
           <h2>Problem 2</h2>
           <p>Content for the second problem goes here...</p>
         </section>
--- src/components/cases/BtnsToRedirect.js
+++ src/components/cases/BtnsToRedirect.js
@@ -1,14 +1,21 @@
 "use client";
-import { NavLink} from '@mantine/core';
-import { IconChevronRight } from '@tabler/icons-react';
+import { NavLink } from "@mantine/core";
+import { IconChevronRight } from "@tabler/icons-react";
 import styles from "./Cases.module.css";
 
-
-export default function Home({titles}) {
-   const sections = [
-    { name: "Problema 1", description: "Description of problem ......", anchor: "#problema-1" },
-    { name: "Problem 2", description: "Description of problem ......", anchor: "#problema-2" }
-  ]; 
+export default function Home({ titles }) {
+  const sections = [
+    {
+      name: "Problema 1",
+      description: "Description of problem ......",
+      anchor: "#problema-1",
+    },
+    {
+      name: "Problem 2",
+      description: "Description of problem ......",
+      anchor: "#problema-2",
+    },
+  ];
 
   return (
     <div className={styles.btns}>
@@ -24,9 +31,9 @@ export default function Home({titles}) {
           variant="filled"
           styles={{
             root: {
-              borderRadius: '8px',
-              marginBottom: '4px',
-            }
+              borderRadius: "8px",
+              marginBottom: "4px",
+            },
           }}
         />
       ))}
