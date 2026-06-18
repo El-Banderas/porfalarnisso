1fab52e38d682d31ed3146b64cd7f4c64eaf5438
El-Banderas <El-Banderas@users.noreply.github.com>
2026-06-18 20:00:19 +0000

    style: format files

--- src/components/cases/Case.js
+++ src/components/cases/Case.js
@@ -3,14 +3,12 @@ import styles from "./Cases.module.css";
 import Image from "next/image";
 
 export default function Case({ id, section }) {
-  
-
   return (
     <div className={styles.case} id={id}>
       <h1>{section.name}</h1>
       <h4>{section.description}</h4>
 
       <Image src={section.image} alt={section.title} className={styles.img} />
-  </div>
+    </div>
   );
 }
